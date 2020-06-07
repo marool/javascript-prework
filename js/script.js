2a12a04964ae385fe9655cec460b560c2ceb3871
Marcin Kaczmarek <mkmarol@gmail.com>
2020-06-07 18:56:28 +0200

    Add 2,3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+let computerMove = 'papier', 'nożyce';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+if(playerInput == '2'){
+  playerMove = 'papier';
+}
+if(playerInput == '3'){
+  playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
