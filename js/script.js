19a2699d460c0a4e4b7622494f1fc8ff69dd76f4
Marcin Kaczmarek <mkmarol@gmail.com>
2020-06-08 20:35:45 +0200

    Edit 2,3

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'papier', 'nożyce';
+let computerMove = 'papier';
 
 if(randomNumber == 1){
   computerMove = 'kamień';
@@ -14,3 +14,21 @@ if(randomNumber == 3){
   computerMove = 'nożyce';
 }
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'papier';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+if(playerInput == '2'){
+  playerMove = 'papier';
+}
+if(playerInput == '3'){
+  playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
