5d944ee85a89197d85ea457eb1da5f44d50a5358
Marcin Kaczmarek <mkmarol@gmail.com>
2020-06-14 12:40:04 +0200

    Add argMoveId

--- js/script.js
+++ js/script.js
@@ -2,15 +2,21 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'papier';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if(randomNumber == 2){
-  computerMove = 'papier';
-} else if(randomNumber == 3){
-  computerMove = 'nożyce';
+function getMoveName(argMoveId){
+  if (argMoveId == 1){
+    return 'kamień';
+  } else if (argMoveId == 2) {
+  	return 'papier';
+  } else if (argMoveId == 3) {
+  	return 'nożyce';
+  } else {
+  	printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
 }
+
 printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
