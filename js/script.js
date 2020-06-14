56a2738a34b059b8a152dd96ffbde92d7bce1f3e
Marcin Kaczmarek <mkmarol@gmail.com>
2020-06-14 13:26:02 +0200

    Add displayresult

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = getMoveName(randomNumber);
 
+printMessage('Komputer wylosował: ' + computerMove);
+
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+let playerMove = getMoveName(playerInput);
+
+printMessage('Gracz wylosował: ' + playerMove);
+
 function getMoveName(argMoveId){
   if (argMoveId == 1){
     return 'kamień';
@@ -17,35 +25,22 @@ function getMoveName(argMoveId){
   }
 }
 
-printMessage('Mój ruch to: ' + computerMove);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if(playerInput == '2'){
-  playerMove = 'papier';
-} else if(playerInput == '3'){
-  playerMove = 'nożyce';
-}
-
-printMessage('Twój ruch to: ' + playerMove);
-
-
-if( computerMove == 'kamień' && playerMove == 'papier'){
+function displayResult(argComputerMove, argPlayerMove){
+	console.log('moves:', argComputerMove, argPlayerMove);
+	if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
   printMessage('Ty wygrywasz!');
-} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+} else if(argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
   printMessage('Ty wygrywasz!');
-} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+} else if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
   printMessage('Ty wygrywasz!');
-} else if( computerMove == playerMove){
+} else if(argComputerMove == argPlayerMove){
   printMessage('Remis!');
-} else if( playerMove === 'nieznany ruch'){
-    printMessage('Spróbuj jeszcze raz')
+} else if(argPlayerMove === 'nieznany ruch'){
+  printMessage('Spróbuj jeszcze raz');
 } else {
   printMessage('Przegrywasz!');
-} 
+}
+}
+printMessage('Wynik: ' + displayResult(argComputerMove, argPlayerMove));
+
+
