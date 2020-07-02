bc670d1669eab68f2a5a93a469f5606c66d9259c
Marcin Kaczmarek <mkmarol@gmail.com>
2020-07-02 11:05:43 +0200

    edit const-let

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
+{
 function playGame(playerInput) {
 
 	clearMessages();
 
-		let randomNumber = Math.floor(Math.random() * 3 + 1);
+		const randomNumber = Math.floor(Math.random() * 3 + 1);
 
 		console.log('Wylosowana liczba to: ' + randomNumber);
 
-		let computerMove = getMoveName(randomNumber);
+		const computerMove = getMoveName(randomNumber);
 
 		printMessage('Komputer wylosował: ' + computerMove);
 
 		/*playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
 
-		let playerMove = getMoveName(playerInput);
+		const playerMove = getMoveName(playerInput);
 
 		printMessage('Gracz wylosował: ' + playerMove);
 
@@ -60,5 +61,5 @@ function playGame(playerInput) {
  document.getElementById('play-scissors').addEventListener('click', function(){
 	(playGame(3));
 });
-
+}
 
